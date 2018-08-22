8c4079aa2109c26a8533db7ba47945c0596b132a
Trin Jette <trinjette@gmail.com>
2018-08-22 19:42:25 -0400

    navbar

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  sidebar = '';
   title = 'my-app';
+
+  constructor() {
+  
+  }
+
 }
+
+
