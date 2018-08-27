c77651b1c243349bdb49211b742969a9155f8d39
Trin Jette <trinjette@gmail.com>
2018-08-26 22:08:36 -0400

    allNavigation

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,30 +1,60 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('dropdowncont1') dropdown1Container;
+  @ViewChild('dropdowncont2') dropdown2Container;
 
   sidebar = 'off';
   title = 'my-app';
   dropdown = false;
+  index = 0;
+  titlebox = true;
+  overflow = 'no-overflow'
+  mobileOpen = true;
 
-  constructor() {
-  
+  constructor(private _eref: ElementRef) {
+    document.addEventListener('click', this.offClickHandler.bind(this));
   }
 
+  toggleMobileNav() {
+    this.mobileOpen = !this.mobileOpen;
+  }
+
+  /* Closing Dropdown */
   toggleDropdown() {
     this.dropdown = !this.dropdown;
+    if (!this.dropdown) {
+      this.titlebox = false;
+      setTimeout( ()=> this.titlebox = true, 600)
+    }
+  }
+
+  /* This function can probably be combined with toggleDropdown */
+  offClickHandler(event:any) {
+    if (!this.dropdown1Container.nativeElement.contains(event.target) && !this.dropdown2Container.nativeElement.contains(event.target)) {
+        this.dropdown = false;
+        this.titlebox = false;
+        setTimeout( ()=> this.titlebox = true, 600)
+    }
   }
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event) {
     if (window.pageYOffset > 500) {
       this.sidebar = 'on';
-    } else {
+      setTimeout( ()=> {
+        if (window.pageYOffset > 500) {
+          this.overflow = 'overflow'
+        }
+      }, 600)
+    } else if (window.pageYOffset <= 500) {
       this.sidebar = 'off';
+      this.overflow = 'no-overflow';
     }
   }
 
